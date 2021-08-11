b41b6c33aeeb4b45ccf51a26f721d47555b9bdf1
SEUNG HWAN WON <seunghwanwon@SEUNGs-MacBook-Pro.local>
2021-08-11 19:52:26 +0900

    hello world

---
+++ src/main/java/cc/qwer/QwerApplication.java
@@ -0,0 +1,13 @@
+package cc.qwer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QwerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QwerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cc/qwer/QwerApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.qwer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QwerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
